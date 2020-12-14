57b52222880b6408485df689b3d67c28f287c8c8
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-12-14 11:01:31 -0800

    commit tree 62722fe4ef0d0a81b99194352518009285093c16

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students, only: [:index, :show]
+
+  get '/student/:id/activate', to: 'students#active', as: :activate_student
 end
